6e34891c40a271cc5fa181e4acda790f739a80d0
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-01-27 20:28:29 +0100

    Initial Commit

---
+++ SignUp.Login/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SignUp.Login
+//
+//  Created by Flama Henkel on 27.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SignUp.Login/SignUp_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUp_LoginApp.swift
+//  SignUp.Login
+//
+//  Created by Flama Henkel on 27.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SignUp_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
