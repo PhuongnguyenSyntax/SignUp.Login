712487c7fcaef7d0c311de4e4f4839cbefd3ed08
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-01-27 23:01:20 +0100

    RegistrationView

---
+++ SignUp.Login/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  SignUp.Login
+//
+//  Created by Flama Henkel on 27.01.24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
---
+++ SignUp.Login/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  SignUp.Login
+//
+//  Created by Flama Henkel on 27.01.24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
